8ba837769df4ef205cd6e28b1d12c008c52b9084
Maiconfz <maiconfz@gmail.com>
2023-10-07 01:55:37 +0100

    Init spring project

---
+++ src/main/java/io/github/maiconfz/javasoliddesignprinciples/App.java
@@ -0,0 +1,13 @@
+package io.github.maiconfz.javasoliddesignprinciples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSolidDesignPrinciplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSolidDesignPrinciplesApplication.class, args);
+	}
+
+}
